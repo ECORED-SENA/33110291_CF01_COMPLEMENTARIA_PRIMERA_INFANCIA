fd24f3801f283dc76931469aacdb5a15a2c5a961
Camilo Bolaño <80546125+elrilacho@users.noreply.github.com>
2023-08-14 17:19:47 -0500

    material.zip

--- src/config/global.js
+++ src/config/global.js
@@ -114,11 +114,11 @@ export default {
         titulo: 'Descargar PDF',
         download: 'downloads/33110291_CF01_DU.pdf',
       },
-      // {
-      //   icono: 'fas fa-download',
-      //   titulo: 'Descargar material',
-      //   download: 'downloads/material.zip',
-      // },
+      {
+        icono: 'fas fa-download',
+        titulo: 'Descargar material',
+        download: 'downloads/material.zip',
+      },
       {
         icono: 'far fa-registered',
         titulo: 'Créditos',
